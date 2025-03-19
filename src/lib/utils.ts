8dd31d196d8ee7f82bda139ebefe697d1802fbe1
danynurf <bydany.nf@gmail.com>
2025-03-19 10:20:29 +0700

    feat (util): generate seat per class

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,28 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { TypeSeat } from "@prisma/client";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export async function generateSeatPerClass(flightId: string) {
+  const SEAT_CLASSES: TypeSeat[] = ["ECONOMY", "BUSINESS", "FIRST"];
+  const SEAT_CODE = ["A", "B", "C", "D"];
+
+  const seats: { flightId: string; type: TypeSeat; seatNumber: string }[] = [];
+
+  for (const type of SEAT_CLASSES) {
+    for (const code of SEAT_CODE) {
+      for (let index = 1; index <= 5; index++) {
+        seats.push({
+          flightId,
+          type,
+          seatNumber: code + index,
+        });
+      }
+    }
+  }
+  
+  return seats;
 }
