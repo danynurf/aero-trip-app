27c4e0e309001cba7ef334c8d6bc831d67fe2d79
danynurf <bydany.nf@gmail.com>
2025-03-20 09:40:05 +0700

    feat (utils): format date, format rupiah, mapping seats

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
-import { TypeSeat } from "@prisma/client";
+import { FlightSeat, TypeSeat } from "@prisma/client";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import dayjs from "dayjs";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -26,3 +27,39 @@ export async function generateSeatPerClass(flightId: string) {
   
   return seats;
 }
+
+export const dateFormat = (date: Date | string, format: string = "DD MMM YYYY") => {
+  if (!date) return "";
+
+  const day = dayjs(date);
+
+  if (!day.isValid()) return "";
+
+  return day.format(format);
+};
+
+export const formatRupiah = (amount: number) => {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+  }).format(amount);
+};
+
+export const mappingSeats = (seats: FlightSeat[]) => {
+  const economySeats = seats.filter((seat) => seat.type == "ECONOMY");
+  const businessSeats = seats.filter((seat) => seat.type == "BUSINESS");
+  const firstSeats = seats.filter((seat) => seat.type == "FIRST");
+
+  const economySeatsAvailable = economySeats.filter((seat) => !seat.isBooked).length;
+  const businessSeatsAvailable = businessSeats.filter((seat) => !seat.isBooked).length;
+  const firstSeatsAvailable = firstSeats.filter((seat) => !seat.isBooked).length;
+
+  return {
+    economySeatsAvailable,
+    businessSeatsAvailable,
+    firstSeatsAvailable,
+    economySeatsTotal: economySeats.length,
+    businessSeatsTotal: businessSeats.length,
+    firstSeatsTotal: firstSeats.length,
+  };
+};
