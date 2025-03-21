5b2b9779d348d374451d1c7d5d88fd4ba3106010
danynurf <bydany.nf@gmail.com>
2025-03-21 08:14:56 +0700

    feat (actions): add update and delete func

--- src/app/dashboard/(pages)/flights/lib/actions.ts
+++ src/app/dashboard/(pages)/flights/lib/actions.ts
@@ -37,3 +37,50 @@ export async function createFlight(prevState: unknown, formData: FormData): Prom
   revalidatePath("/dashboard/flights");
   redirect("/dashboard/flights");
 }
+
+export async function updateFlight(prevState: unknown, id: string, formData: FormData): Promise<ActionResult> {
+  const validatedFields = validateFlight.safeParse({
+    ...Object.fromEntries(formData),
+    departureDate: new Date(formData.get("departureDate") as string),
+    arrivalDate: new Date(formData.get("arrivalDate") as string),
+  });
+
+  if (!validatedFields.success) {
+    return {
+      errorTitle: "Invalid fields",
+      errorMessage: validatedFields.error.issues.map((issue) => issue.message).join(", "),
+    };
+  }
+
+  await prisma.flight.update({
+    where: { id },
+    data: {
+      ...validatedFields.data,
+      price: Number(validatedFields.data.price),
+    },
+  });
+
+  revalidatePath("/dashboard/flights");
+  redirect("/dashboard/flights");
+}
+
+export async function deleteFlight(id: string): Promise<ActionResult> {
+  try {
+    await prisma.flightSeat.deleteMany({
+      where: { flightId: id },
+    });
+    
+    await prisma.flight.delete({
+      where: { id },
+    });
+  } catch (error) {
+    console.error(error);
+    return {
+      errorTitle: "Failed to delete flight",
+      errorMessage: "Failed to delete flight",
+    };
+  }
+
+  revalidatePath("/dashboard/flights");
+  redirect("/dashboard/flights");
+}
